6bf8706bff5d9ee0b5828ee8c41cc62a6ed88f2b
Seungkyu-Han <trust1204@gmail.com>
2026-03-29 00:10:04 +0900

    test: add test code

---
+++ tests/test_event_bus.py
@@ -0,0 +1,40 @@
+from unittest.mock import Mock
+
+import pytest
+
+from event_pubsub import EventBus
+
+
+def test_event_bus_create_correctly():
+    # then
+    EventBus()
+
+def test_event_bus_subscribe_correctly():
+    # given
+    test_handler = lambda x: ...
+    test_event_type = 'event_type'
+
+    # when
+    event_bus = EventBus()
+
+    # then
+    event_bus.subscribe(test_event_type, test_handler)
+
+
+@pytest.mark.asyncio
+async def test_event_bus_emit_correctly():
+    # given
+    event_bus = EventBus()
+    test_result: list[str] = []
+    test_message = "hello"
+
+    async def test_handler(array: list[str], data: str):
+        array.append(data)
+
+    event_bus.subscribe("test_event", test_handler)
+
+    # when
+    await event_bus.emit("test_event", test_result, test_message)
+
+    # then
+    assert test_result == [test_message]
