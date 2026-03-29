4c3b4fde57cc4321bcc79e200648e0020dfc656a
Seungkyu-Han <trust1204@gmail.com>
2026-03-29 21:19:11 +0900

    test: add branch sync function test

--- tests/test_event_bus.py
+++ tests/test_event_bus.py
@@ -9,6 +9,7 @@ def test_event_bus_create_correctly():
     # then
     EventBus()
 
+
 def test_event_bus_subscribe_correctly():
     # given
     test_handler = lambda x: ...
@@ -29,6 +30,25 @@ async def test_event_bus_emit_correctly():
     test_message = "hello"
 
     async def test_handler(array: list[str], data: str):
+        array.append(data)
+
+    event_bus.subscribe("test_event", test_handler)
+
+    # when
+    await event_bus.emit("test_event", test_result, test_message)
+
+    # then
+    assert test_result == [test_message]
+
+
+@pytest.mark.asyncio
+async def test_event_bus_emit_sync_func_correctly():
+    # given
+    event_bus = EventBus()
+    test_result: list[str] = []
+    test_message = "hello"
+
+    def test_handler(array: list[str], data: str):
         array.append(data)
 
     event_bus.subscribe("test_event", test_handler)
