4e0d406d96236ca06c6ef6c99e6938c1b2b33d85
Seungkyu-Han <trust1204@gmail.com>
2026-03-29 21:17:11 +0900

    refactor: change member to private

--- event_pubsub/publisher.py
+++ event_pubsub/publisher.py
@@ -3,7 +3,7 @@ from event_pubsub.event_bus import EventBus
 
 class Publisher:
     def __init__(self, event_bus: EventBus):
-        self.event_bus = event_bus
+        self._event_bus = event_bus
 
     async def publish(self, event_type: str, data: any):
-        await self.event_bus.emit(event_type, data)
+        await self._event_bus.emit(event_type, data)
--- event_pubsub/subscriber.py
+++ event_pubsub/subscriber.py
@@ -3,10 +3,10 @@ from event_pubsub.event_bus import EventBus
 
 class Subscriber:
     def __init__(self, event_bus: EventBus):
-        self.event_bus = event_bus
+        self._event_bus = event_bus
 
     def subscribe(self, event_type: str):
         def decorator(handler: callable):
-            self.event_bus.subscribe(event_type, handler)
+            self._event_bus.subscribe(event_type, handler)
             return handler
         return decorator
